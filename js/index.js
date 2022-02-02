296e37a404771fa3f36fcbc8ee937f4c5fcf9889
Darrow <nicedarrow12@gmail.com>
2022-02-01 22:03:04 -0300

    🟡 Anotações

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+console.log("Estou em outro arquivo");
+
 var titulo = document.querySelector(".tituloAN");
 
 titulo.textContent = "Banana";
+
+/*
+  * O navegador, ao ler o seu arquivo HTML, cria uma cópia em memória daquele HTML e a partir dessa cópia ele vai desenhando a sua página, colocando as tags e aplicando os estilos. Esta cópia é o que chamamos de DOM uma representação em memória do HTML do seu arquivo, que o navegador usa para desenhar a página, e a variável document é quem contêm o DOM.
+
+  * Por isso, quando dizemos que com o Javascript nós estamos manipulando o DOM, estamos manipulando o que o navegador renderizou. Então ao trocar algum texto do DOM, o navegador imediatamente desenha novamente aquele texto, pois o DOM é o que o navegador usa para desenhar o seu site.
+
+  * Outra caracteristica interessante, é que como manipulamos o DOM , quando trocamos um texto de um <h1> ou um estilo de um elemento, na verdade estamos alterando a representação em memória , o que faz com que o arquivo fonte que contêm seu HTML fique intacto!
+*/
