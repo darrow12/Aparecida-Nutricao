cb01fd0efbef4d4cbb300f85f320a23c4fce02e8
Darrow <nicedarrow12@gmail.com>
2022-03-05 19:26:55 -0300

    🟡 Selecionado todos os pacientes e imprimindo-os

--- js/index.js
+++ js/index.js
@@ -1,58 +1,36 @@
-// Catching the ID of the first patient
-const paciente = document.querySelector("#primeiro-paciente");
+// Catching the ID of all the patients in HTML
+const pacientes = document.querySelectorAll(".paciente");
 
-// Catching the weight of the patient
-const tdPeso = paciente.querySelector(".info-peso");
-const peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
 
-// Catching the height of the patient
-const tdAltura = paciente.querySelector(".info-altura");
-const altura = tdAltura.textContent;
-
-const resultadoIMC = paciente.querySelector(".info-imc");
-
-let pesoValido = true;
-let alturaValida = true;
-
-function ValidarPesoAltura() {
+  // Catching the weight of the patient
+  const tdPeso = paciente.querySelector(".info-peso");
+  const peso = tdPeso.textContent;
+  
+  // Catching the height of the patient
+  const tdAltura = paciente.querySelector(".info-altura");
+  const altura = tdAltura.textContent;
+  
+  const resultadoIMC = paciente.querySelector(".info-imc");
+  
+  let pesoValido = true;
+  let alturaValida = true;
+  
   if (peso <= 0 || peso >= 1000) {
     console.log("Peso inválido!");
     pesoValido = false;
     resultadoIMC.textContent = "Peso inválido!";
   }
-
+  
   if (altura <= 0 || altura >= 3.0) {
     console.log("Altura inválida!");
     alturaValida = false;
     resultadoIMC.textContent = "Altura inválida!";
   }
-}
-
-ValidarPesoAltura();
-
-function ResultadoIMC() {
+  
   if (pesoValido && alturaValida) {
     const imc = peso / (altura * altura);
-    resultadoIMC.textContent = imc;
+    resultadoIMC.textContent = imc.toFixed(2);
   }
 }
-
-ResultadoIMC();
-
-
-// if (peso <= 0 || peso >= 1000) {
-//   console.log("Peso inválido!");
-//   pesoValido = false;
-//   resultadoIMC.textContent = "Peso inválido!";
-// }
-
-// if (altura <= 0 || altura >= 3.0) {
-//   console.log("Altura inválida!");
-//   alturaValida = false;
-//   resultadoIMC.textContent = "Altura inválida!";
-// }
-
-// if (pesoValido && alturaValida) {
-//   const imc = peso / (altura * altura);
-//   resultadoIMC.textContent = imc;
-// }
